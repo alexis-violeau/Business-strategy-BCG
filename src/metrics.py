545c5ed0fca346c4772a3d8380dc19ed08777aa1
alexis-violeau <alexis.violeau.98@gmail.com>
2022-02-09 15:47:25 +0100

    add business performance metrics

--- src/metrics.py
+++ src/metrics.py
@@ -7,7 +7,11 @@ def model_performance(model,df,churn,promotion_ratio = 0.1):
     r = 0.005
     
     y_pred = model.predict(df)
-    costs = y_pred * df['mean_sales'] * promotion_ratio
-    rewards = y_pred * churn * df['mean_sales'] * df['n_orders'] / df['days_client'] * 365 * (1+r)/r
     
+    # When predicting churn, we offer a promotion corresponding to a share of its next order
+    costs = y_pred * df['mean_sales'] * promotion_ratio
+    
+    # If we correctly predicted churn, our promotion makes him stay for at least one year
+    rewards = (y_pred * churn) * df['mean_sales'] * df['n_orders'] / df['duration'] * 365
+        
     return rewards - costs
