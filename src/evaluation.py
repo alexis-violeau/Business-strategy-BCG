39e420f9cc08c8eb4199cc72ec8940f77cfed51e
alexis-violeau <alexis.violeau.98@gmail.com>
2022-02-10 14:50:29 +0100

    adjust colorplot

--- src/evaluation.py
+++ src/evaluation.py
@@ -4,9 +4,10 @@ from src import modeling
 import matplotlib.pyplot as plt
 
 def evaluate_model(model,X_val,y_val):
+    plt.style.use('dark_background')
     y_pred = model.predict(X_val)
     plt.title('Confusion matrix')
-    sns.heatmap(confusion_matrix(y_val,y_pred,normalize = 'all'),annot = True,cmap="YlGnBu")
+    sns.heatmap(confusion_matrix(y_val,y_pred,normalize = 'all'),annot = True,cmap="YlGn")
     plt.xlabel('Prediction')
     plt.ylabel('Ground truth')
     plt.xticks([0.5,1.5],['No churn','churn'])
